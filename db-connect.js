e4d91cbb5fc5ed66aee8025884b26b8460d73375
vishank91 <vishankchauhan@gmail.com>
2024-06-17 14:11:09 +0530

    17-06-2024

--- db-connect.js
+++ db-connect.js
@@ -3,7 +3,7 @@ async function getConnect() {
     try {
         // await mongoose.connect("mongodb://localhost:27017/may_24_1pm_server")
         await mongoose.connect(process.env.DB_KEY)
-        console.log("Database is Connected")
+        console.log("Database is Connected") 
     } catch (error) {
         console.log(error)
     }
